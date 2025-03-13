74f01050491a1c5722282f8cd2938fc3edb496cd
Yaan-Moraes <yanmoraes@dafiti.com.br>
2025-03-12 23:13:03 -0300

    k6

--- k6/index.js
+++ k6/index.js
@@ -5,7 +5,13 @@ import createActivityScenario from './scenarios/createActivityScenario.js';  // 
 import deleteActivityScenario from './scenarios/deleteActivityScenario.js';  // Cenário de requisição DELETE (deletar atividade)
 import getActivityByIdScenario from './scenarios/getActivityByIdScenario.js';  // Cenário de requisição GET (atividade específica)
 import updateActivityScenario from './scenarios/updateActivityScenario.js';  // Cenário de requisição PUT (atualizar atividade)
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+}
 
 export default function () {
     activitiesScenario();
